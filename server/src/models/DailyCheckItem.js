f5b1cb1604a34190c2c2babf79b4a062cef76a81
radiohead0803-hash <radiohead0803@gmail.com>
2025-11-27 13:18:41 +0900

    fix: DailyCheckItem 모델 export 방식 수정 - 클래스 기반에서 함수 기반 export로 변경 - Sequelize 초기화 에러 해결

--- server/src/models/DailyCheckItem.js
+++ server/src/models/DailyCheckItem.js
@@ -1,84 +1,84 @@
 const { Model, DataTypes } = require('sequelize');
 
-class DailyCheckItem extends Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
-        check_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'daily_checks',
-            key: 'id'
-          }
-        },
-        item_master_id: {
-          type: DataTypes.INTEGER,
-          references: {
-            model: 'check_item_master',
-            key: 'id'
-          }
-        },
-        category: {
-          type: DataTypes.STRING(50)
-        },
-        item_name: {
-          type: DataTypes.STRING(100)
-        },
-        item_description: {
-          type: DataTypes.TEXT
-        },
-        status: {
-          type: DataTypes.STRING(20),
-          comment: '양호, 주의, 불량'
-        },
-        notes: {
-          type: DataTypes.TEXT
-        },
-        photos: {
-          type: DataTypes.JSONB,
-          comment: '사진 [{url, caption, uploaded_at}]'
-        },
-        is_required: {
-          type: DataTypes.BOOLEAN,
-          defaultValue: true
-        },
-        display_order: {
-          type: DataTypes.INTEGER
+module.exports = (sequelize) => {
+  class DailyCheckItem extends Model {
+    static associate(models) {
+      DailyCheckItem.belongsTo(models.DailyCheck, {
+        foreignKey: 'check_id',
+        as: 'check'
+      });
+
+      DailyCheckItem.belongsTo(models.CheckItemMaster, {
+        foreignKey: 'item_master_id',
+        as: 'itemMaster'
+      });
+    }
+  }
+
+  DailyCheckItem.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      check_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'daily_checks',
+          key: 'id'
         }
       },
-      {
-        sequelize,
-        modelName: 'DailyCheckItem',
-        tableName: 'daily_check_items',
-        timestamps: false,
-        createdAt: 'created_at',
-        updatedAt: false,
-        underscored: true,
-        indexes: [
-          { fields: ['check_id'] },
-          { fields: ['item_master_id'] }
-        ]
+      item_master_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'check_item_master',
+          key: 'id'
+        }
+      },
+      category: {
+        type: DataTypes.STRING(50)
+      },
+      item_name: {
+        type: DataTypes.STRING(100)
+      },
+      item_description: {
+        type: DataTypes.TEXT
+      },
+      status: {
+        type: DataTypes.STRING(20),
+        comment: '양호, 주의, 불량'
+      },
+      notes: {
+        type: DataTypes.TEXT
+      },
+      photos: {
+        type: DataTypes.JSONB,
+        comment: '사진 [{url, caption, uploaded_at}]'
+      },
+      is_required: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+      },
+      display_order: {
+        type: DataTypes.INTEGER
       }
-    );
-  }
+    },
+    {
+      sequelize,
+      modelName: 'DailyCheckItem',
+      tableName: 'daily_check_items',
+      timestamps: false,
+      createdAt: 'created_at',
+      updatedAt: false,
+      underscored: true,
+      indexes: [
+        { fields: ['check_id'] },
+        { fields: ['item_master_id'] }
+      ]
+    }
+  );
 
-  static associate(models) {
-    this.belongsTo(models.DailyCheck, {
-      foreignKey: 'check_id',
-      as: 'check'
-    });
-
-    this.belongsTo(models.CheckItemMaster, {
-      foreignKey: 'item_master_id',
-      as: 'itemMaster'
-    });
-  }
-}
-
-module.exports = DailyCheckItem;
+  return DailyCheckItem;
+};
