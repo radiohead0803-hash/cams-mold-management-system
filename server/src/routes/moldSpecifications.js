0924c1f5d134101d72cb8f4056ac267e08bbb1de
radiohead0803-hash <radiohead0803@gmail.com>
2025-11-27 11:50:12 +0900

    feat: 금형 사양 수정/삭제 권한 추가 - mold_developer, system_admin만 수정 가능 - 부품사진 업로드/삭제 권한 추가 - 권한 없는 사용자 접근 차단

--- server/src/routes/moldSpecifications.js
+++ server/src/routes/moldSpecifications.js
@@ -24,16 +24,16 @@ router.get('/', getMoldSpecifications);
 // 금형 사양 상세 조회
 router.get('/:id', getMoldSpecificationById);
 
-// 금형 사양 수정
-router.patch('/:id', updateMoldSpecification);
+// 금형 사양 수정 (금형개발 담당만 가능)
+router.patch('/:id', authorize(['mold_developer', 'system_admin']), updateMoldSpecification);
 
-// 금형 사양 삭제
-router.delete('/:id', deleteMoldSpecification);
+// 금형 사양 삭제 (금형개발 담당만 가능)
+router.delete('/:id', authorize(['mold_developer', 'system_admin']), deleteMoldSpecification);
 
-// 부품 사진 업로드 (단일 이미지)
-router.post('/:id/part-image', uploadSingle, uploadPartImage);
+// 부품 사진 업로드 (금형개발 담당만 가능)
+router.post('/:id/part-image', authorize(['mold_developer', 'system_admin']), uploadSingle, uploadPartImage);
 
-// 부품 사진 삭제
-router.delete('/:id/part-image', deletePartImage);
+// 부품 사진 삭제 (금형개발 담당만 가능)
+router.delete('/:id/part-image', authorize(['mold_developer', 'system_admin']), deletePartImage);
 
 module.exports = router;
