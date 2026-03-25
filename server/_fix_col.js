3e31e7fa98733f9f20378e8c6855da37575d4a6f
radiohead0803-hash <radiohead0803@example.com>
2026-03-25 15:51:06 +0900

    chore: 임시 스크립트 삭제

--- server/_fix_col.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-const c = new Client('postgresql://postgres:YcdaEiRCsgzeWWgAcrfzmkQuXZDYShMd@switchyard.proxy.rlwy.net:34950/railway');
-c.connect().then(async () => {
-  await c.query(`ALTER TABLE maintenance_records ADD COLUMN IF NOT EXISTS photos JSONB DEFAULT '[]'::jsonb`);
-  console.log('photos 컬럼 추가 완료');
-  await c.end();
-}).catch(e => { console.error(e.message); c.end(); });
